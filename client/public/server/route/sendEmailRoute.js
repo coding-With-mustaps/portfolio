6c6aee345a75d45811b6cd8ee2149c8c18f37970
Mustapha <akinti.munira85@gmail.com>
2024-10-26 15:39:05 +0000

    Finally for deployment

--- client/public/server/route/sendEmailRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const controller = require("../controller/sendEmailController");
-
-const router = Router();
-
-router.post("/v1/send-email", controller.sendEmail);
-
-module.exports = router;
